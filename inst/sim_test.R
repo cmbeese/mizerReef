660dd1829f2aaf95f4663ae368295778e3d5ce6a
Chelsey Beese <shellsea.beese@gmail.com>
2024-02-29 06:14:48 +1300

    tester script to check if degradation and plotProd are working

--- inst/sim_test.R
+++ inst/sim_test.R
@@ -11,27 +11,27 @@ params <- karpata_model
 
 
 # Normal
-sim1 <- project(params, tmax = 10) # works fine
-b <- plotBiomass(sim1, start_time = 1, end_time = 10, total = TRUE, 
-                 return_data = TRUE)
-p <- plotProductivity(sim1, start_time = 1, end_time = 10, 
-                      return_data = TRUE) # also works
-df <- mizer::plotBiomass(sim1,total = TRUE, return_data = TRUE)
+sim1 <- project(params, t_max = 30) # works fine
+b <- plotBiomass(sim1, total = TRUE,  return_data = TRUE)
+p <- plotProductivity(sim1, return_data = TRUE) # also works
+print(plotBiomass(sim1))
 
 # Add carrying capacity
 params2 <- setURcapacity(params)
 params2 <- reefSteady(params2)
-sim2 <- project(params2, tmax = 10) # this also works
-b <- plotBiomass(sim2, start_time = 1, end_time = 10)
-p <- plotProductivity(sim2, start_time = 1, end_time = 10)
+sim2 <- project(params2, t_max = 30) # this also works
+b <- plotBiomass(sim2)
+p <- plotProductivity(sim2)
+print(b)
+print(p)
 
 # Add degradation without carrying capacity
 params3 <- setDegradation(params, trajectory = "rubble", 
                           deg_scale = constant_scale)
 params3 <- reefSteady(params3)
-sim3 <- project(params3, tmax = 10)
-b <- plotBiomass(sim3, start_time = 1, end_time = 10)
-p <- plotProductivity(sim3, start_time = 1, end_time = 10)
+sim3 <- project(params3, t_max = 30)
+b <- plotBiomass(sim3)
+p <- plotProductivity(sim3)
 print(b)
 print(p)
 
@@ -40,8 +40,9 @@ params4 <- setDegradation(params, trajectory = "rubble",
                           deg_scale = rubble_scale)
 params4 <- setURcapacity(params4)
 params4 <- reefSteady(params4)
-sim4 <- project(params4, tmax = 10)
-b <- plotBiomass(sim4, start_time = 1, end_time = 10)
-p <- plotProductivity(sim4, start_time = 1, end_time = 10)
+params4 <- reefSteady(params4)
+sim4 <- project(params4, t_max = 30)
+b <- plotBiomass(sim4)
+p <- plotProductivity(sim4)
 print(b)
 print(p)
